edea24eeecced65ade8735533f4efd064cd80d11
natalicio almeida <natoctba@hotmail.com>
2024-12-03 21:26:47 -0300

    Entity Client

--- src/main/java/com/nato/dsjsp_modulo3/entities/Client.java
+++ src/main/java/com/nato/dsjsp_modulo3/entities/Client.java
@@ -1,4 +1,86 @@
 package com.nato.dsjsp_modulo3.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Description cannot be empty")
+    private String name;
+    private String cpf;
+    private Double income;
+    @PastOrPresent(message = "Birth date cannot be in the future")
+    private LocalDate birthDate;
+    private Integer children;
+
+
+    public Client() {    }
+
+
+    public Client(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+
 }
