4a4c7927f434107c1791d945d0dfa3d093df421e
natalicio almeida <natoctba@hotmail.com>
2024-12-03 21:28:13 -0300

    client repository

---
+++ src/main/java/com/nato/dsjsp_modulo3/repositories/ClientRepository.java
@@ -0,0 +1,8 @@
+package com.nato.dsjsp_modulo3.repositories;
+
+import com.nato.dsjsp_modulo3.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
